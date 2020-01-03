1fc17cdfb6e6b870913f1f47ee71e51bba736448
bo <boyu0107@gmail.com>
2020-01-02 22:32:51 -0500

    module directive

--- pm/src/app/main/directives/dashboard-module.directive.ts
+++ pm/src/app/main/directives/dashboard-module.directive.ts
@@ -1,7 +1,29 @@
 import { HeatmapComponent } from '../components/dashboard/modules/heatmap/heatmap.component';
 import { AgetStateComponent } from '../components/dashboard/modules/aget-state/aget-state.component';
 import { AgentStateComponent } from '../components/dashboard/modules/agent-state/agent-state.component';
+import { Directive, OnChanges, Input, ComponentRef, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
+
 export const moduleComponents = {
-    heatmap: HeatmapComponent,
-    agentState: AgentStateComponent
+	heatmap: HeatmapComponent,
+	agentState: AgentStateComponent
+};
+
+@Directive({
+	selector: '[dashboardModule]'
+})
+export class DashboardModuleDirective implements OnChanges {
+	@Input() componentRef: string;
+
+	component: ComponentRef<any>;
+
+    constructor(private container: ViewContainerRef, private resolver: ComponentFactoryResolver) {}
+    
+    ngOnChanges(e): void {
+        const component = moduleComponents[this.componentRef];
+        if (component) {
+            const factory = this.resolver.resolveComponentFactory<any>(component);
+            this.component = this.container.createComponent(factory);
+            // TODO config injection
+        }
+    }
 }
