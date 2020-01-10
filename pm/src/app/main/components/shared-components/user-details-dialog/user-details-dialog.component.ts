aa8bc1fc6190913ca94829517d0546394060bc2c
bo <boyu0107@gmail.com>
2020-01-09 22:17:40 -0500

    before editing

--- pm/src/app/main/components/shared-components/user-details-dialog/user-details-dialog.component.ts
+++ pm/src/app/main/components/shared-components/user-details-dialog/user-details-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { UserAdminDTO } from '../../user-admin/user-admin.component';
+import { UserAdminDTO, Role } from '../../user-admin/user-admin.component';
 import { UserAdminService } from '../../../services/user-admin.service';
 
 @Component({
@@ -14,12 +14,15 @@ export class UserDetailsDialogComponent implements OnInit, OnDestroy {
   imgSrc: string = '';
   profileForEdit: UserAdminDTO;
   profileEditing: boolean = false;
+  roleList: Role[];
 
 	constructor(
 		private uas: UserAdminService,
 		public dialogRef: MatDialogRef<UserDetailsDialogComponent>,
 		@Inject(MAT_DIALOG_DATA) public data: UserAdminDTO
-	) {}
+	) {
+    this.uas.getRoles().subscribe(res => this.roleList = res, err => console.log(err));
+  }
 
 	closeDialog(): void {
 		this.dialogRef.close();
@@ -28,6 +31,19 @@ export class UserDetailsDialogComponent implements OnInit, OnDestroy {
   editProfile() {
     this.profileForEdit = JSON.parse(JSON.stringify(this.data));
     this.profileEditing = true;
+    console.log(this.roleList);
+    
+  }
+
+  cancelEditing() {
+    this.profileEditing = null;
+    this.profileEditing = false;
+  }
+
+  submitEditing() {
+    console.log(this.profileForEdit);
+    this.profileEditing = false;
+    this.profileEditing = null; 
   }
 
 	replaceImage(e) {
--- pm/src/app/main/services/user-admin.service.ts
+++ pm/src/app/main/services/user-admin.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { UserAdminDTO } from '../components/user-admin/user-admin.component';
@@ -8,71 +8,82 @@ import { UserDetailsDialogComponent } from '../components/shared-components/user
 import { ImageCropComponent } from '../components/shared-components/image-crop/image-crop.component';
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
 export class UserAdminService {
+	constructor(private http: HttpClient, private dialog: MatDialog) {}
 
-    constructor(private http: HttpClient, private dialog: MatDialog) {}
+	getAllUsers(): Observable<any> {
+		return this.http.get(`${environment.API_URL}/user-admin/users`, { withCredentials: true });
+	}
 
-    getAllUsers(): Observable<any> {
-        return this.http.get(`${environment.API_URL}/user-admin/users`, {withCredentials: true})
-    }
-
-    openUserAdminDialog(param) {
+	openUserAdminDialog(param) {
 		// open dialog
 		const dialogConfig = new MatDialogConfig();
 
 		dialogConfig.disableClose = false;
 		dialogConfig.autoFocus = true;
 		dialogConfig.width = '100vw';
-        dialogConfig.height = '90vh';
-        dialogConfig.data = param;
-        
-        const dialogRef = this.dialog.open(UserDetailsDialogComponent, dialogConfig);
+		dialogConfig.height = '90vh';
+		dialogConfig.data = param;
+
+		const dialogRef = this.dialog.open(UserDetailsDialogComponent, dialogConfig);
+	}
+
+	openImageCropDialog(param: UserAdminDTO): Observable<any> {
+		const dialogConfig = new MatDialogConfig();
+
+		dialogConfig.disableClose = true;
+		dialogConfig.autoFocus = true;
+		dialogConfig.width = '30vw';
+		dialogConfig.height = '50vh';
+		dialogConfig.minHeight = '300px';
+		dialogConfig.minWidth = '300px';
+		dialogConfig.data = { src: param.userInfo.profileImage };
+
+		const dialogRef = this.dialog.open(ImageCropComponent, dialogConfig);
+		return dialogRef.afterClosed();
     }
-
-    openImageCropDialog(param: UserAdminDTO): Observable<any> {
-        const dialogConfig = new MatDialogConfig();
-
-        dialogConfig.disableClose = true;
-        dialogConfig.autoFocus = true;
-        dialogConfig.width = '30vw';
-        dialogConfig.height = '50vh';
-        dialogConfig.minHeight = '300px';
-        dialogConfig.minWidth =  '300px';
-        dialogConfig.data = {src: param.userInfo.profileImage};
-
-        const dialogRef = this.dialog.open(ImageCropComponent, dialogConfig);
-        return dialogRef.afterClosed()
-    }
-    b64toBlob(dataURI) {
-
-        var byteString = atob(dataURI.split(',')[1]);
-        var ab = new ArrayBuffer(byteString.length);
-        var ia = new Uint8Array(ab);
     
-        for (var i = 0; i < byteString.length; i++) {
-            ia[i] = byteString.charCodeAt(i);
-        }
-        return new Blob([ab], { type: 'image/jpeg' });
+	b64toBlob(dataURI) {
+		var byteString = atob(dataURI.split(',')[1]);
+		var ab = new ArrayBuffer(byteString.length);
+		var ia = new Uint8Array(ab);
+
+		for (var i = 0; i < byteString.length; i++) {
+			ia[i] = byteString.charCodeAt(i);
+		}
+		return new Blob([ ab ], { type: 'image/jpeg' });
     }
-    uploadImage(data, userId) {
-        const config = {
-            reportProgress: true, 
-            withCredential: true,
-            headers: {'Content-Type' : 'undefined',
-                        'Access-Control-Allow-Credentials' : true},
-        };
+    
+	uploadImage(data, userId) {
+		const config = {
+			reportProgress: true,
+			withCredential: true,
+			headers: {
+				'Content-Type': 'undefined',
+				'Access-Control-Allow-Credentials': true
+			}
+		};
 
-        let formData: FormData = new FormData();
-        formData.append("data", this.b64toBlob(data) , "asdf.jpg");
-        
-        formData.append("userId", userId);
-        const options = {
-            headers: new HttpHeaders().append('Access-Control-Allow-Credentials', 'true')
-        }
+		let formData: FormData = new FormData();
+		formData.append('data', this.b64toBlob(data), 'asdf.jpg');
 
-        this.http.post(`${environment.API_URL}/user-admin/upload-image`, formData, {withCredentials: true}).subscribe(res=> console.log(res), err => console.log(err));
+		formData.append('userId', userId);
+		const options = {
+			headers: new HttpHeaders().append('Access-Control-Allow-Credentials', 'true')
+		};
 
+		this.http
+			.post(`${environment.API_URL}/user-admin/upload-image`, formData, { withCredentials: true })
+			.subscribe((res) => console.log(res), (err) => console.log(err));
+    }
+    
+    getRoles(): Observable<any> {
+        return this.http.get(`${environment.API_URL}/user-admin/roles`);
+    }
+
+    getGroupLevels(): Observable<any> {
+        return this.http.get(`${environment.API_URL}/user-admin/group-level`);
     }
 }
