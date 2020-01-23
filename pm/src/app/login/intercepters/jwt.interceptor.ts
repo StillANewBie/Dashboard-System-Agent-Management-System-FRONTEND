07de72b549d5eb06687f0daa4b916da40512ac53
bo <boyu0107@gmail.com>
2020-01-23 10:13:35 -0500

    remove console.log

--- pm/src/app/login/intercepters/jwt.interceptor.ts
+++ pm/src/app/login/intercepters/jwt.interceptor.ts
@@ -11,7 +11,6 @@ export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let currentUser = this.authenticationService.currentUserValue;
     if (currentUser && currentUser.token) {
-      console.log(`Bearer ${currentUser.token}`)
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${currentUser.token}`
